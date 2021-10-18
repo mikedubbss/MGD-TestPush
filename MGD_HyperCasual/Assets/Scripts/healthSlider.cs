08c6d1e95866814d6bee8c056613044d4cc9ee58
Michael <mwest6@student.fitchburgstate.edu>
2021-10-18 00:09:43 -0400

    Newest Unity Build
    I'm sorry I forgot to push my newest files before midnight!  I hope it doesn't reflect poorly on my grade.  I did get the actual build turned into my student file before midnight though so that should count as something right? hahaha

---
+++ MGD_HyperCasual/Assets/Scripts/healthSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthSlider : MonoBehaviour
+{
+
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealthBar(float health)
+    {
+        slider.value = health;
+    }
+}
---
+++ MGD_HyperCasual/Assets/Scripts/loopingBackground.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class loopingBackground : MonoBehaviour
+{
+    private Vector2 startPos;
+    private double repeatWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider2D>().size.y / 0.78;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < startPos.y - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+}
